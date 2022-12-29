cc6d76f5d57245172373deaff5e36a0c64a0fac0
Slater <armando.jaimes@pucp.edu.pe>
2022-12-28 22:31:25 -0500

    Mensajes de conexión y desconexión - Cliente

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,2 +1,24 @@
+// Referencias del html
+
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
 
 const socket = io(); // Esto expone la librería ./socket.io/socket.io.js    
+
+
+socket.on('connect', () => {
+  console.log('Conectado cliente');
+
+  lblOffline.style.display = 'none';
+  lblOnline.style.display  = '';
+  
+});
+
+
+socket.on('disconnect', () => {
+  console.log('Desconectado del servidor!!!!');
+  lblOnline.style.display  = 'none';
+  lblOffline.style.display = '';
+  
+});
